340a97f9beb434b878fd3302e8f115a45d64bbfe
Omkar Sanjay Narad <79644475+omkar2120@users.noreply.github.com>
2022-01-29 23:32:44 +0530

    Update Cart.js

--- frontend/src/component/Cart.js
+++ frontend/src/component/Cart.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import Navbar from './Navbar';
+import Navbar from './Navbar';                     
 import "./cart.css"
 import { useSelector , useDispatch } from 'react-redux';
 import { useEffect } from 'react';
